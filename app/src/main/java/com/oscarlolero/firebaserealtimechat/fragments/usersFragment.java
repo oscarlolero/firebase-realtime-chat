761f859dcb50c444687d05663ccc664fc6046dad
Oscar Montes <oscmcojc1@live.com.mx>
2021-06-11 12:48:29 -0500

    Show registered user

--- app/src/main/java/com/oscarlolero/firebaserealtimechat/fragments/usersFragment.java
+++ app/src/main/java/com/oscarlolero/firebaserealtimechat/fragments/usersFragment.java
@@ -7,7 +7,12 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.oscarlolero.firebaserealtimechat.R;
 
 public class usersFragment extends Fragment {
@@ -15,7 +20,18 @@ public class usersFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_users, container, false);
+        final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        View view = inflater.inflate(R.layout.fragment_users, container, false);
+
+        TextView tvUser = view.findViewById(R.id.tvUser);
+        ImageView imgUser = view.findViewById(R.id.imgUser);
+
+        assert user != null;
+        tvUser.setText(user.getDisplayName());
+
+        Glide.with(this).load(user.getPhotoUrl()).into(imgUser);
+
+        return view;
     }
 }
